b1fda8cfd1841ec5d7edd45cb9172995cf652d13
Sam D <samidiaz3@gmail.com>
2021-01-13 16:27:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aieouAIEOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/^un\zing/)
 end
 
 def words_five_letters_long(text)
