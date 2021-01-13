beba5bfbdda003c0db742d386f3f485a967ee35f
Sam D <samidiaz3@gmail.com>
2021-01-13 18:04:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match (/\S\d{10}/) ? true : false
+  phone.match (/\d{10}|(.\d{3}.\d{3}.\d{4})|(.\d{3}.\d{7})|(.\b\d{12})/) ? true : false
 end
